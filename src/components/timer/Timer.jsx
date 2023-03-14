1df2b271d96063b437f1f9bca7b0e53a7b82642c
mohamedsahal <mohaansahal@gmail.com>
2023-03-14 09:25:50 +0300

    styling the timer component with sass

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -4,24 +4,28 @@ const Timer = () => {
     <div className={styles.timer}>
         <h1>VOTING ENDS IN</h1>
         <div className={styles.times}>
-            <div className={styles.digit_text}></div>
+            <div className={styles.digit_text}>
             <span className={styles.digit}>08</span>
             <span className={styles.text}>Days</span>
-        </div>
-        <div className={styles.times}>
-            <div className={styles.digit_text}></div>
+            </div>
+       
+      
+            <div className={styles.digit_text}>
             <span className={styles.digit}>08</span>
-            <span className={styles.text}>Days</span>
-        </div>
-        <div className={styles.times}>
-            <div className={styles.digit_text}></div>
+            <span className={styles.text}>Hours</span>
+            </div>
+        
+      
+            <div className={styles.digit_text}>
             <span className={styles.digit}>08</span>
-            <span className={styles.text}>Days</span>
-        </div>
-        <div className={styles.times}>
-            <div className={styles.digit_text}></div>
+            <span className={styles.text}>Minutes</span>
+            </div>
+        
+      
+            <div className={styles.digit_text}>
             <span className={styles.digit}>08</span>
-            <span className={styles.text}>Days</span>
+            <span className={styles.text}>Seconds</span>
+            </div>
         </div>
     </div>
   )
